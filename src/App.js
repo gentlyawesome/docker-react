df4c9ddc7a4626c4ff96f76319d21b0ef91fea58
gentlyawesome <gentlyawesome@gmail.com>
2020-03-23 11:29:34 +0800

    fixed test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Ahoy! Matety!</h2>
+        <h2>Ahoy!</h2>
       </header>
     </div>
   );
